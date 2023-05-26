a5f50cb160c5ae17d7cbb0668ce6b2f4a4041b8b
Thegan Govender <govenderthegan@gmail.com>
2023-05-26 19:28:12 +0200

    Update sync.js

--- backend/sync.js
+++ backend/sync.js
@@ -64,7 +64,13 @@ class sync {
           "X-MediaBrowser-Token": this.apiKey,
         },
       });
-      console.l
+      if(!response || typeof response.data !== 'object' || !Array.isArray(response.data))
+      {
+        res.status(503);
+        res.send({ error: "Invalid Response from Users API Call.", user_response:response });
+        return;
+      }
+  
       const adminUser = response.data.filter(
         (user) => user.Policy.IsAdministrator === true
       );
