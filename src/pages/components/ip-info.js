3089cb2c3f384b1a323e373badb7560d3b8bcdd3
fuzz <>
2023-11-03 18:01:28 +0000

    use table-style for modal
    more consistent with stream info modal from activity table

--- src/pages/components/ip-info.js
+++ src/pages/components/ip-info.js
@@ -1,16 +1,49 @@
 import React from "react";
 import Loading from "./general/loading";
-import Modal from 'react-bootstrap/Modal';
+import { Button, Modal } from "react-bootstrap";
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 export default function IpInfoModal(props) {
     let modalBody = <Loading/>;
 
     if(props.geodata) {
-        modalBody = <Modal.Body className="card">
-                        <p className="card-text">City: {props.geodata.city.names['en']}</p>
-                        <p className="card-text">Country: {props.geodata.country.names['en']}</p>
-                        <p className="card-text">Postcode: {props.geodata.postal.code}</p>
-                        <p className="card-text">ISP: {props.geodata.traits.autonomous_system_organization}</p>
+        modalBody = <Modal.Body>
+                        <div className="StreamInfo"> 
+                            <TableContainer className="overflow-hidden">
+                                <Table aria-label="collapsible table" >
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell/>
+                                            <TableCell></TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell className="py-0 pb-1">City</TableCell>
+                                            <TableCell>{props.geodata.city.names['en']}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell className="py-0 pb-1">Country</TableCell>
+                                            <TableCell>{props.geodata.country.names['en']}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell className="py-0 pb-1">Postcode</TableCell>
+                                            <TableCell>{props.geodata.postal.code}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell className="py-0 pb-1">ISP</TableCell>
+                                            <TableCell>{props.geodata.traits.autonomous_system_organization}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </div>
                     </Modal.Body>
     }
 
@@ -21,9 +54,13 @@ export default function IpInfoModal(props) {
                     <Modal.Title>
                         Geolocation info for {props.ipAddress}
                     </Modal.Title>
-                </Modal.Header>
-                
+                </Modal.Header>                
                 {modalBody}
+                <Modal.Footer>
+                    <Button variant="outline-primary" onClick={()=>props.onHide()}>
+                        Close
+                    </Button>
+                </Modal.Footer>
             </Modal>
         </div>
     );
