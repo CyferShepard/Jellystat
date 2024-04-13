80dad5ea8642cc56b6bd8c6e14cc38584c4ed954
CyferShepard <govenderthegan@gmail.com>
2024-04-13 17:03:34 +0200

    possible fix for pb_activity duplicate insert

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,16 @@
-const { Pool } = require('pg');
-const pgp = require('pg-promise')();
-const {
-  update_query: update_query_map,
-} = require('./models/bulk_insert_update_handler');
+const { Pool } = require("pg");
+const pgp = require("pg-promise")();
+const { update_query: update_query_map } = require("./models/bulk_insert_update_handler");
+const moment = require("moment");
 
 const _POSTGRES_USER = process.env.POSTGRES_USER;
 const _POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 const _POSTGRES_IP = process.env.POSTGRES_IP;
 const _POSTGRES_PORT = process.env.POSTGRES_PORT;
-const _POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || 'jfstat';
+const _POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || "jfstat";
 
-if (
-  [_POSTGRES_USER, _POSTGRES_PASSWORD, _POSTGRES_IP, _POSTGRES_PORT].includes(
-    undefined
-  )
-) {
-  console.log('Error: Postgres details not defined');
+if ([_POSTGRES_USER, _POSTGRES_PASSWORD, _POSTGRES_IP, _POSTGRES_PORT].includes(undefined)) {
+  console.log("Error: Postgres details not defined");
   return;
 }
 
@@ -27,18 +22,18 @@ const pool = new Pool({
   port: _POSTGRES_PORT,
 });
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
   return;
   //process.exit(-1);
 });
 
 async function deleteBulk(table_name, data) {
   const client = await pool.connect();
-  let result = 'SUCCESS';
-  let message = '';
+  let result = "SUCCESS";
+  let message = "";
   try {
-    await client.query('BEGIN');
+    await client.query("BEGIN");
 
     if (data && data.length !== 0) {
       const deleteQuery = {
@@ -48,27 +43,27 @@ async function deleteBulk(table_name, data) {
       await client.query(deleteQuery);
     }
 
-    await client.query('COMMIT');
-    message = data.length + ' Rows removed.';
+    await client.query("COMMIT");
+    message = data.length + " Rows removed.";
   } catch (error) {
-    await client.query('ROLLBACK');
-    message = 'Bulk delete error: ' + error;
-    result = 'ERROR';
+    await client.query("ROLLBACK");
+    message = "Bulk delete error: " + error;
+    result = "ERROR";
   } finally {
     client.release();
   }
-  return { Result: result, message: '' + message };
+  return { Result: result, message: "" + message };
 }
 
-async function updateSingleFieldBulk(table_name, data,field_name, new_value, where_field) {
+async function updateSingleFieldBulk(table_name, data, field_name, new_value, where_field) {
   const client = await pool.connect();
-  let result = 'SUCCESS';
-  let message = '';
-  if(where_field===undefined || where_field===null || where_field===''){
-    where_field='Id'
+  let result = "SUCCESS";
+  let message = "";
+  if (where_field === undefined || where_field === null || where_field === "") {
+    where_field = "Id";
   }
   try {
-    await client.query('BEGIN');
+    await client.query("BEGIN");
 
     if (data && data.length !== 0) {
       const updateQuery = {
@@ -78,16 +73,16 @@ async function updateSingleFieldBulk(table_name, data,field_name, new_value, whe
       await client.query(updateQuery);
     }
 
-    await client.query('COMMIT');
-    message = data.length + ' Rows updated.';
+    await client.query("COMMIT");
+    message = data.length + " Rows updated.";
   } catch (error) {
-    await client.query('ROLLBACK');
-    message = 'Bulk update error: ' + error;
-    result = 'ERROR';
+    await client.query("ROLLBACK");
+    message = "Bulk update error: " + error;
+    result = "ERROR";
   } finally {
     client.release();
   }
-  return { Result: result, message: '' + message };
+  return { Result: result, message: "" + message };
 }
 
 async function insertBulk(table_name, data, columns) {
@@ -95,13 +90,32 @@ async function insertBulk(table_name, data, columns) {
 
   if (Array.isArray(data)) {
     data = data.reduce((accumulator, currentItem) => {
-      const isDuplicate = accumulator.some((item) =>
-        currentItem.Id
-          ? item.Id === currentItem.Id
-          : item.rowid === currentItem.rowid
+      const isNotDuplicate = !accumulator.some((item) =>
+        currentItem.Id ? item.Id === currentItem.Id : item.rowid === currentItem.rowid
       );
 
-      if (!isDuplicate) {
+      let isNotPbDuplicate = true;
+      if (table_name.toLowerCase() === "jf_playback_activity") {
+        let uniqueItems = accumulator.filter(
+          (item, index, self) =>
+            index ===
+            self.findIndex(
+              (t) =>
+                moment(t.ActivityDateInserted, "YYYY-MM-DD HH:mm:ss.SSSZ")
+                  .millisecond(0)
+                  .isSame(moment(item.ActivityDateInserted, "YYYY-MM-DD HH:mm:ss.SSSZ").millisecond(0)) &&
+                t.UserId === item.UserId &&
+                t.NowPlayingItemId === item.NowPlayingItemId &&
+                t.SeasonId === item.SeasonId &&
+                t.EpisodeId === item.EpisodeId &&
+                t.PlaybackDuration === item.PlaybackDuration &&
+                t.DeviceId === item.DeviceId
+            )
+        );
+        isNotPbDuplicate = uniqueItems.length == 0 ? true : false;
+      }
+
+      if (isNotDuplicate && isNotPbDuplicate) {
         accumulator.push(currentItem);
       }
 
@@ -111,27 +125,25 @@ async function insertBulk(table_name, data, columns) {
 
   //
   const client = await pool.connect();
-  let result = 'SUCCESS';
-  let message = '';
+  let result = "SUCCESS";
+  let message = "";
   try {
-    await client.query('BEGIN');
-    const update_query = update_query_map.find(
-      (query) => query.table === table_name
-    ).query;
-    await client.query('COMMIT');
+    await client.query("BEGIN");
+    const update_query = update_query_map.find((query) => query.table === table_name).query;
+    await client.query("COMMIT");
     const cs = new pgp.helpers.ColumnSet(columns, { table: table_name });
     const query = pgp.helpers.insert(data, cs) + update_query; // Update the column names accordingly
     await client.query(query);
   } catch (error) {
-    await client.query('ROLLBACK');
-    message = '' + error;
-    result = 'ERROR';
+    await client.query("ROLLBACK");
+    message = "" + error;
+    result = "ERROR";
   } finally {
     client.release();
   }
   return {
     Result: result,
-    message: message ? 'Bulk insert error: ' + message : '',
+    message: message ? "Bulk insert error: " + message : "",
   };
 }
 
@@ -140,29 +152,18 @@ async function query(text, params) {
     const result = await pool.query(text, params);
     return result;
   } catch (error) {
-    if (error?.routine === 'auth_failed') {
-      console.log('Error 401: Unable to Authenticate with Postgres DB');
-    } else if (error?.code === 'ENOTFOUND') {
-      console.log('Error: Unable to Connect to Postgres DB');
-    } else if (error?.code === 'ERR_SOCKET_BAD_PORT') {
-      console.log(
-        'Error: Invalid Postgres DB Port Range. Port should be >= 0 and < 65536.'
-      );
-    } else if (error?.code === 'ECONNREFUSED') {
-      console.log(
-        'Error: Postgres DB Connection refused at ' +
-          error.address +
-          ':' +
-          error.port
-      );
+    if (error?.routine === "auth_failed") {
+      console.log("Error 401: Unable to Authenticate with Postgres DB");
+    } else if (error?.code === "ENOTFOUND") {
+      console.log("Error: Unable to Connect to Postgres DB");
+    } else if (error?.code === "ERR_SOCKET_BAD_PORT") {
+      console.log("Error: Invalid Postgres DB Port Range. Port should be >= 0 and < 65536.");
+    } else if (error?.code === "ECONNREFUSED") {
+      console.log("Error: Postgres DB Connection refused at " + error.address + ":" + error.port);
     } else {
-
-      if(error.message && !error.message.includes('database "' + _POSTGRES_DATABASE + '" does not exist'))
-      {
-        console.error('[JELLYSTAT]: Error occurred while executing query:', error.message);
+      if (error.message && !error.message.includes('database "' + _POSTGRES_DATABASE + '" does not exist')) {
+        console.error("[JELLYSTAT]: Error occurred while executing query:", error.message);
       }
-     
-     
     }
     return [];
     // throw error;
@@ -173,6 +174,6 @@ module.exports = {
   query: query,
   deleteBulk: deleteBulk,
   insertBulk: insertBulk,
-  updateSingleFieldBulk:updateSingleFieldBulk,
+  updateSingleFieldBulk: updateSingleFieldBulk,
   // initDB: initDB,
 };
