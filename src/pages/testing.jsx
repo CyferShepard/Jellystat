6ba8a3188a13346bfbca4d3e4266d5fd222762c4
CyferShepard <govenderthegan@gmail.com>
2024-10-04 09:25:07 +0200

    random change to test caching in github actions

--- src/pages/testing.jsx
+++ src/pages/testing.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Sessions from "./debugTools/sessions";
 import PlayMethodStats from "./components/statistics/playbackMethodStats";
-import PlaybackMethodStats from "./components/statCards/playback_method_stats";
+// import PlaybackMethodStats from "./components/statCards/playback_method_stats";
 
 const TestingRoutes = () => {
   return (
     <Routes>
       <Route path="/sessions" element={<Sessions />} />
       <Route path="/stats" element={<PlayMethodStats />} />
-      <Route path="/statsstream" element={<PlaybackMethodStats />} />
+      {/* <Route path="/statsstream" element={<PlaybackMethodStats />} /> */}
     </Routes>
   );
 };
