c31edc045a6b38a14d40219bcc3c6ed28ff3aa38
CyferShepard <govenderthegan@gmail.com>
2024-10-07 10:13:44 +0200

    added ENV overides for JF_HOST and JF_API_KEY

--- backend/classes/config.js
+++ backend/classes/config.js
@@ -3,6 +3,10 @@ const db = require("../db");
 class Config {
   async getConfig() {
     try {
+      //Manual overrides
+      process.env.POSTGRES_USER = process.env.POSTGRES_USER ?? "postgres";
+
+      //
       const { rows: config } = await db.query('SELECT * FROM app_config where "ID"=1');
 
       const state = this.#getConfigState(config);
@@ -12,8 +16,8 @@ class Config {
       }
 
       return {
-        JF_HOST: config[0].JF_HOST,
-        JF_API_KEY: config[0].JF_API_KEY,
+        JF_HOST: process.env.JF_HOST ?? config[0].JF_HOST,
+        JF_API_KEY: process.env.JF_API_KEY ?? config[0].JF_API_KEY,
         APP_USER: config[0].APP_USER,
         APP_PASSWORD: config[0].APP_PASSWORD,
         REQUIRE_LOGIN: config[0].REQUIRE_LOGIN,
