50a4f23e638055b1405ed592a51e8ff920d73726
CyferShepard <govenderthegan@gmail.com>
2024-10-15 09:35:52 +0200

    added controll to toggle global stats to show defaulted 180 and 365 to not shown

--- src/pages/components/general/globalStats.jsx
+++ src/pages/components/general/globalStats.jsx
@@ -4,6 +4,8 @@ import "../../css/globalstats.css";
 
 import WatchTimeStats from "./globalstats/watchtimestats";
 import { Trans } from "react-i18next";
+import { Checkbox, FormControlLabel, IconButton, Menu } from "@mui/material";
+import { GridMoreVertIcon } from "@mui/x-data-grid";
 
 function GlobalStats(props) {
   const [dayStats, setDayStats] = useState({});
@@ -12,11 +14,56 @@ function GlobalStats(props) {
   const [d180Stats, setd180Stats] = useState({});
   const [d365Stats, setd365Stats] = useState({});
   const [allStats, setAllStats] = useState({});
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [prefs, setPrefs] = useState(
+    localStorage.getItem("PREF_GLOBAL_STATS") != undefined ? JSON.parse(localStorage.getItem("PREF_GLOBAL_STATS")) : [180, 365]
+  );
+
   const token = localStorage.getItem("token");
 
-  const [prefs, setPrefs] = useState(
-    localStorage.getItem("PREF_GLOBAL_STATS") != undefined ? JSON.parse(localStorage.getItem("PREF_GLOBAL_STATS")) : []
-  );
+  const stats = [
+    {
+      key: 1,
+      heading: <Trans i18nKey="GLOBAL_STATS.LAST_24_HRS" />,
+      data: dayStats,
+      setMethod: setDayStats,
+    },
+    {
+      key: 7,
+      heading: <Trans i18nKey="GLOBAL_STATS.LAST_7_DAYS" />,
+      data: weekStats,
+      setMethod: setWeekStats,
+    },
+    {
+      key: 30,
+      heading: <Trans i18nKey="GLOBAL_STATS.LAST_30_DAYS" />,
+      data: monthStats,
+      setMethod: setMonthStats,
+    },
+    {
+      key: 180,
+      heading: <Trans i18nKey="GLOBAL_STATS.LAST_180_DAYS" />,
+      data: d180Stats,
+      setMethod: setd180Stats,
+    },
+    {
+      key: 365,
+      heading: <Trans i18nKey="GLOBAL_STATS.LAST_365_DAYS" />,
+      data: d365Stats,
+      setMethod: setd365Stats,
+    },
+    {
+      key: 9999,
+      heading: <Trans i18nKey="GLOBAL_STATS.ALL_TIME" />,
+      data: allStats,
+      setMethod: setAllStats,
+    },
+  ];
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   function fetchStats(hours = 24, setMethod = setDayStats) {
     axios
@@ -41,12 +88,9 @@ function GlobalStats(props) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (!prefs.includes("1")) fetchStats(24, setDayStats);
-        if (!prefs.includes("7")) fetchStats(24 * 7, setWeekStats);
-        if (!prefs.includes("30")) fetchStats(24 * 30, setMonthStats);
-        if (!prefs.includes("180")) fetchStats(24 * 180, setd180Stats);
-        if (!prefs.includes("365")) fetchStats(24 * 365, setd365Stats);
-        if (!prefs.includes("ALL")) fetchStats(24 * 999, setAllStats);
+        for (let i = 0; i < stats.length; i++) {
+          if (!prefs.includes(stats[i].key)) fetchStats(24 * stats[i].key, stats[i].setMethod);
+        }
       } catch (error) {
         console.log(error);
       }
@@ -57,16 +101,74 @@ function GlobalStats(props) {
     return () => clearInterval(intervalId);
   }, [props.id, token]);
 
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  function toggleStat(stat) {
+    let newPrefs = prefs;
+    if (newPrefs.includes(stat.key)) {
+      newPrefs = newPrefs.filter((item) => item !== stat.key);
+      fetchStats(24 * stat.key, stat.setMethod);
+    } else {
+      newPrefs = [...newPrefs, stat.key];
+    }
+    setPrefs(newPrefs);
+    localStorage.setItem("PREF_GLOBAL_STATS", JSON.stringify(newPrefs));
+  }
+
   return (
     <div>
-      <h1 className="py-3">{props.title}</h1>
+      <div className="d-flex flex-row justify-content-between">
+        <h1 className="py-3">{props.title}</h1>
+        <IconButton aria-label="more" aria-controls="long-menu" aria-haspopup="true" onClick={handleMenuOpen}>
+          <GridMoreVertIcon />
+        </IconButton>
+
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleMenuClose}
+          sx={{
+            "& .MuiPaper-root": {
+              backgroundColor: `var(--background-color)`,
+              color: "#fff",
+              minWidth: "200px",
+            },
+            "& .MuiMenuItem-root": {
+              "&:hover": {
+                backgroundColor: "#555",
+              },
+            },
+          }}
+        >
+          {stats.map((stat) => {
+            // return <MenuItem key={stat.key}>{stat.heading}</MenuItem>;
+            return (
+              <div key={stat.key} style={{ padding: "10px" }}>
+                <FormControlLabel
+                  control={<Checkbox checked={!prefs.includes(stat.key)} onChange={() => toggleStat(stat)} name={stat.heading} />}
+                  label={stat.heading}
+                />
+              </div>
+            );
+          })}
+        </Menu>
+        {/* <Menu id="long-menu" anchorEl={anchorEl} keepMounted open={open} onClose={handleMenuClose}>
+        {stats.map((stat) => {
+            <MenuItem>{stat.heading}Test</MenuItem>;
+          })}
+          <MenuItem onClick={handleMenuClose}>Option 1</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Option 2</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Option 3</MenuItem>
+        </Menu> */}
+      </div>
       <div className="global-stats-container">
-        {!prefs.includes("1") && <WatchTimeStats data={dayStats} heading={<Trans i18nKey="GLOBAL_STATS.LAST_24_HRS" />} />}
-        {!prefs.includes("7") && <WatchTimeStats data={weekStats} heading={<Trans i18nKey="GLOBAL_STATS.LAST_7_DAYS" />} />}
-        {!prefs.includes("30") && <WatchTimeStats data={monthStats} heading={<Trans i18nKey="GLOBAL_STATS.LAST_30_DAYS" />} />}
-        {!prefs.includes("180") && <WatchTimeStats data={d180Stats} heading={<Trans i18nKey="GLOBAL_STATS.LAST_180_DAYS" />} />}
-        {!prefs.includes("365") && <WatchTimeStats data={d365Stats} heading={<Trans i18nKey="GLOBAL_STATS.LAST_365_DAYS" />} />}
-        {!prefs.includes("ALL") && <WatchTimeStats data={allStats} heading={<Trans i18nKey="GLOBAL_STATS.ALL_TIME" />} />}
+        {stats.map((stat) => {
+          if (!prefs.includes(stat.key)) return <WatchTimeStats data={stat.data} heading={stat.heading} />;
+        })}
       </div>
     </div>
   );
