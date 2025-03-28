f583b68d96a4b87d80cc4cc17286ee64517e3d74
Patrick <49989644+botagas@users.noreply.github.com>
2025-03-28 03:09:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,8 @@ const ensureSlashes = (url) => {
   return url;
 };
 
-const PORT = process.env.PORT;
-const LISTEN_IP = "0.0.0.0";
+const PORT = process.env.PORT || 3000;
+const LISTEN_IP = process.env.LISTEN_IP || "0.0.0.0";
 const JWT_SECRET = process.env.JWT_SECRET;
 const BASE_NAME = process.env.JS_BASE_URL ? ensureSlashes(process.env.JS_BASE_URL) : "";
 
