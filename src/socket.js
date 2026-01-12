76fe771a94d45bf293cb1b0e37deb2b62299f335
cellscape <me@nooff.info>
2026-01-12 07:38:36 +0700

    Honor base URL when making websocket connection from browser
    socket.io docs[1] say that path in URI represents namespace but actual
    path is provided via options.
    
    Ref #488
    
    [1] https://socket.io/docs/v4/client-options/#path

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 import baseUrl from "./lib/baseurl";
 
-const socket = io(baseUrl, {
-  path: "/socket.io",
+const socket = io("/", {
+  path: baseUrl + "/socket.io",
   auth: {
     token: localStorage.getItem("token"),
   },
